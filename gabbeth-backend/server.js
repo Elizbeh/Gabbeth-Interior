27354fbca340a482ca7c190566f5747b068d954f
Elizbeh <eliz01_01@yahoo.com>
2023-02-08 13:36:47 +0100

    Adds user and product model to server

--- gabbeth-backend/server.js
+++ gabbeth-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('./connection');
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
@@ -10,9 +11,15 @@ const io = new Server(server, {
 })
 
 
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
 
 
 
